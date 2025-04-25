65c4ca08a34634a8c3c70462f6d59bf741e5d22e
Alexander <avb25@duke.edu>
2025-04-25 13:05:49 -0400

    added in.rotate, which can rotate the sheet any angle to do test (problem with this is boundary)

--- sandbox.py
+++ sandbox.py
@@ -1,20 +1,16 @@
-from mpi4py import MPI
 from graphene_classes_deform import GrapheneSheet
 from graphene_classes_deform import Simulation
 import numpy as np
 
-# initialize core usage etc.
-def initialize_rank():
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    return comm, rank
+x_atoms = 60
+y_atoms = 60
+dist = 1.42
+Lz = 3.4
+if x_atoms % 2 == 0:
+    Lx = ((x_atoms / 2) * dist) + (dist * 2 * (x_atoms / 2 - 1)) + dist
+else:
+    Lx = (((x_atoms - 1) / 2) * dist) + (dist * 2 * ((x_atoms + 1) / 2 - 1)) + (dist / 2)
 
-
-comm, rank = initialize_rank()
-
-sheet = GrapheneSheet("data_files/data.60_60_rel1", 60, 60)
-
-test = Simulation(comm, rank, sheet, x_erate=1e-3, y_erate=1e-3, thermo=1000, sim_length=1000)
-
-
-print(test.strength)
+Ly = y_atoms * dist * np.sin(np.deg2rad(60))
+vol = Lx * Ly * Lz
+print(vol)
