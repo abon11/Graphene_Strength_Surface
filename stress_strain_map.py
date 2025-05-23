93c0460bb65491442aa5b592ec3e0501ffdfab9d
Alexander Bonacci <avb25@duke.edu>
2025-05-23 14:43:34 -0400

    added MLPRegressor

--- stress_strain_map.py
+++ stress_strain_map.py
@@ -7,33 +7,34 @@ import pandas as pd
 import local_config
 import os
 import pickle
+import joblib
 
 
 os.environ["NUM_THREADS"] = "8"
 
 df = pd.read_csv(f'{local_config.DATA_DIR}/angle_testing/all_simulations.csv')
 
-model = PySRRegressor(
-    model_selection="best",
-    niterations=1000,
-    binary_operators=["+", "*", "-", "/"],
-    unary_operators=["square", "sqrt", "log"],
-    loss="loss(x, y) = (x - y)^2",
-    verbosity=1,
-    procs=0,  # auto-threaded
-)
-
-# model = MLPRegressor(
-#     hidden_layer_sizes=(64, 64),   # two hidden layers with 64 neurons each
-#     activation='relu',             # ReLU nonlinearity
-#     solver='adam',                 # good for most problems
-#     max_iter=2000,                 # increase if convergence is slow
-#     random_state=42,
-#     verbose=True
+# model = PySRRegressor(
+#     model_selection="best",
+#     niterations=1000,
+#     binary_operators=["+", "*", "-", "/"],
+#     unary_operators=["square", "sqrt", "log"],
+#     loss="loss(x, y) = (x - y)^2",
+#     verbosity=1,
+#     procs=0,  # auto-threaded
 # )
 
+model = MLPRegressor(
+    hidden_layer_sizes=(64, 64),   # two hidden layers with 64 neurons each
+    activation='relu',             # ReLU nonlinearity
+    solver='adam',                 # good for most problems
+    max_iter=2000,                 # increase if convergence is slow
+    random_state=42,
+    verbose=True
+)
+
 X = df[["Strain Rate x", "Strain Rate y", "Strain Rate xy"]].values
-y = df["Theta"].values  # repeat for Sigma_1, Sigma_2 and Theta
+y = df["Sigma_Ratio"].values  # repeat for Sigma_1, Sigma_2 and Theta
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 x_scaler = StandardScaler()
@@ -41,16 +42,15 @@ y_scaler = StandardScaler()
 
 # scale x train, y train, x test for best performance
 X_train_scaled = x_scaler.fit_transform(X_train)
-X_test_scaled = x_scaler.fit_transform(X_test)
+X_test_scaled = x_scaler.transform(X_test)
 
 y_train_scaled = y_scaler.fit_transform(y_train.reshape(-1, 1)).ravel()
-
-# model.fit(X, y)
-# y_pred = model.predict(X_test)
 
 model.fit(X_train_scaled, y_train_scaled)  # fit the model using scaled training data
 y_pred_scaled = model.predict(X_test_scaled)  # predict from the scaled x test data, getting scaled y data
 y_pred = y_scaler.inverse_transform(y_pred_scaled.reshape(-1, 1)).ravel()  # unscale the predicted y, giving us actual predictions
+
+joblib.dump(model, "outputs/model.pkl")
 
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
