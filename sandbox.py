de7f604335ccf3b30699b965c2c90e30b31c5222
Alexander Bonacci <avb25@duke.edu>
2025-07-09 15:06:36 -0400

    angle tests now go for much longer and include negative shear

--- sandbox.py
+++ sandbox.py
@@ -3,68 +3,16 @@ import local_config
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv(f'{local_config.DATA_DIR}/angle_testing/all_simulations.csv')
+for i in range(1, 102):
+    id = str(i).zfill(5)
+    df = pd.read_csv(f'{local_config.DATA_DIR}/rotation_tests/sim{id}/sim{id}.csv')
+    df['Theta'] = df['Theta'].apply(lambda x: min(x, 180 - x))  # keep thetas in bounds
 
-# df["Sigma_Ratio"] = np.abs(df["Sigma_2"] / df["Sigma_1"])
+    plt.plot(df["Timestep"], df["Theta"], color='black', alpha=0.2)
+    plt.title("Theta vs time - Fracture simulation")
+    plt.xlabel("Timesteps")
+    plt.ylabel("Theta")
 
-# df['Theta'] = df['Theta'].apply(lambda x: min(x, 180 - x))
-
-# # Save it back
-# df.to_csv(f'{local_config.DATA_DIR}/angle_testing/all_simulations.csv', index=False)
-
-plt.scatter(df["Sigma_Ratio"], df["Theta"], s=0.5)
-plt.xlabel("Sigma Ratio")
-plt.ylabel("Theta")
-plt.save("ratio_theta_scatter")
-
-plt.hist(df["Strain Rate x"], bins=60, color='skyblue', alpha=0.5, label='x')
-plt.hist(df["Strain Rate y"], bins=60, color='red', alpha=0.5, label='y')
-plt.hist(df["Strain Rate xy"], bins=60, color='grey', alpha=0.5, label='xy')
-
-plt.xlabel("Strain Rate")
-plt.ylabel("Frequency")
+plt.plot([20000, 20000], [0, 90], '--', color='red', label="Dataset Stopping Point")
 plt.legend()
-plt.title("Distribution of Applied Strain Rates")
-plt.savefig("erates.png")
-plt.close()
-
-
-plt.hist(df["Theta"], bins=60, color='skyblue', alpha=0.5, label='theta')
-
-plt.xlabel("Theta")
-plt.ylabel("Frequency")
-plt.legend()
-plt.title("Distribution of Theta")
-plt.savefig("thetas.png")
-plt.close()
-
-
-
-plt.hist(df["Sigma_Ratio"], bins=60, color='skyblue', alpha=0.5, label='ratio')
-
-plt.xlabel("Ratio")
-plt.ylabel("Frequency")
-plt.legend()
-plt.title("Distribution of Sigma Ratio")
-plt.savefig("ratios.png")
-
-
-
-# import numpy as np
-# theta = np.deg2rad(60)
-# cos2, sin2, sincos = np.cos(theta)**2, np.sin(theta)**2, np.sin(theta)*np.cos(theta)
-
-# erate_1 = [0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001]
-# erate_2 = [0, 0.001, 0.0009, 0.0008, 0.0007, 0.0006, 0.0005, 0.0004, 0.0003, 0.0002, 0.0001]
-
-# for e1, e2 in zip(erate_1, erate_2):
-#     x  = e1 * cos2 + e2 * sin2
-#     y  = e2 * cos2 + e1 * sin2
-#     xy = (e1 - e2) * sincos
-#     print(f"{x} {y} {xy}")
-
-# def f(x0, x1, x2):
-#     return (((((x1 * 1.6953) - (np.sqrt(x1) * 0.0072833)) + ((x0 - x1) * 31.825) ** 2) + x2) - (x0 * ((np.sqrt(np.sqrt(x2)) * 2.5234) + -2.0038))) * 4222
-
-# print(f(0.000523804, 7.21375e-05, 3.98339e-05))  # 5.981216118903219
-# print(f(0.000699599, 0.000666074, 0.000168195))  # 9.4895822170042
+plt.savefig("TEST.png")
