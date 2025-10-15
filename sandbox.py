c1642c383a0f7a70b0b74cc2e8910c03433c9812
Alexander Bonacci <avb25@duke.edu>
2025-10-15 14:26:33 -0400

    added interactive plot for alpha and k as functions of theta

--- sandbox.py
+++ sandbox.py
@@ -1,18 +1,9 @@
-import numpy as np
-import pandas as pd
-import local_config
-from filter_csv import parse_defects_json
+# import numpy as np
+# import pandas as pd
+# import local_config
+# from filter_csv import parse_defects_json
 
-d = {"first": '{"DV": 0.5, "SV": 0.5}', "second": 2}
+d = {"first": (2, None), "second": 2}
 
-# print(str(list(parse_defects_json(d["first"]).keys())[0]))
-defs = parse_defects_json(d["first"])
-print(defs)
-
-string = ''
-for defect, size in defs.items():
-    string += f'{defect} {size}%, '
-
-print(string[:-2])
-
-
+if d["first"][0] is not None:
+    print('hi')
