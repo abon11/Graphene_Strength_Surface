08bddbfe31c15f2b1fd5726525aedd20087dca27
Alexander Bonacci <avb25@duke.edu>
2026-09-16 15:54:29 -0400

    updated run manual to work with the potential input

--- run_manual.sh
+++ run_manual.sh
@@ -6,13 +6,13 @@ EMAIL="avb25@duke.edu"
 
 # Configuration
 SLURM_SCRIPT="./run_one.sh"  # must exist
-CORES_PER_JOB=14
+CORES_PER_JOB=8
 
 x_erate=-0.0001
 y_erate=0.001
 xy_erate=0.0
 
-# SHEET_PATH="${SHEET_PATH:-/hpc/home/avb25/Graphene_Strength_Surface/simulation_data/data_files/data.60_60_rel1}"
+SHEET_PATH="${SHEET_PATH:-/hpc/home/avb25/Graphene_Strength_Surface/simulation_data/data_files/data.60_60_rel_airebolight}"
 X_ATOMS="${X_ATOMS:-60}"
 Y_ATOMS="${Y_ATOMS:-60}"
 # DEFECTS="${DEFECTS:-"None"}"
@@ -28,17 +28,18 @@ DETAILED_DATA="${DETAILED_DATA:-false}"
 ANGLE_TESTING="${ANGLE_TESTING:-false}"
 THETA="${THETA:-0}"
 FRACTURE_WINDOW="${FRACTURE_WINDOW:-10}"
-STORAGE_PATH="${STORAGE_PATH:-/hpc/dctrl/avb25/Graphene_Strength_Surface/simulation_data/rebo}"
+STORAGE_PATH="${STORAGE_PATH:-/hpc/dctrl/avb25/Graphene_Strength_Surface/simulation_data/rebo_uniaxial}"
+POTENTIAL="${POTENTIAL:-"airebo_light"}"
 
 submit_simulation() {
     local repeat_sim=$1
 
     if [ -n "$repeat_sim" ]; then
-        sbatch "$SLURM_SCRIPT" "$CORES_PER_JOB" "$SHEET_PATH" "$X_ATOMS" "$Y_ATOMS" "$DEFECTS" "$DEFECT_RANDOM_SEED" "$SIM_LENGTH" "$TIMESTEP" "$THERMO" "$DETAILED_DATA" "$THETA" "$FRACTURE_WINDOW" "$STORAGE_PATH" "$ACCEPT_DUPES" "$ANGLE_TESTING" "$x_erate" "$y_erate" "$xy_erate" "$repeat_sim"
+        sbatch "$SLURM_SCRIPT" "$CORES_PER_JOB" "$SHEET_PATH" "$X_ATOMS" "$Y_ATOMS" "$DEFECTS" "$DEFECT_RANDOM_SEED" "$SIM_LENGTH" "$TIMESTEP" "$THERMO" "$DETAILED_DATA" "$THETA" "$FRACTURE_WINDOW" "$STORAGE_PATH" "$ACCEPT_DUPES" "$ANGLE_TESTING" "$x_erate" "$y_erate" "$xy_erate" "$POTENTIAL" "$repeat_sim"
         echo "REPEATING SIM $repeat_sim."
     else
-        sbatch "$SLURM_SCRIPT" "$CORES_PER_JOB" "$SHEET_PATH" "$X_ATOMS" "$Y_ATOMS" "$DEFECTS" "$DEFECT_RANDOM_SEED" "$SIM_LENGTH" "$TIMESTEP" "$THERMO" "$DETAILED_DATA" "$THETA" "$FRACTURE_WINDOW" "$STORAGE_PATH" "$ACCEPT_DUPES" "$ANGLE_TESTING" "$x_erate" "$y_erate" "$xy_erate"
-        echo "Starting sheet with: x_erate=$x_erate, y_erate=$y_erate, xy_erate=$xy_erate, SHEET_PATH=$SHEET_PATH, X_ATOMS=$X_ATOMS, Y_ATOMS=$Y_ATOMS, DEFECTS=$DEFECTS, DEFECT_RANDOM_SEED=$DEFECT_RANDOM_SEED, ACCEPT_DUPES=$ACCEPT_DUPES, SIM_LENGTH=$SIM_LENGTH, TIMESTEP=$TIMESTEP, THERMO=$THERMO, DETAILED_DATA=$DETAILED_DATA, ANGLE_TESTING=$ANGLE_TESTING, THETA=$THETA, FRACTURE_WINDOW=$FRACTURE_WINDOW, STORAGE_PATH=$STORAGE_PATH"
+        sbatch "$SLURM_SCRIPT" "$CORES_PER_JOB" "$SHEET_PATH" "$X_ATOMS" "$Y_ATOMS" "$DEFECTS" "$DEFECT_RANDOM_SEED" "$SIM_LENGTH" "$TIMESTEP" "$THERMO" "$DETAILED_DATA" "$THETA" "$FRACTURE_WINDOW" "$STORAGE_PATH" "$ACCEPT_DUPES" "$ANGLE_TESTING" "$x_erate" "$y_erate" "$xy_erate" "$POTENTIAL"
+        echo "Starting sheet with: x_erate=$x_erate, y_erate=$y_erate, xy_erate=$xy_erate, SHEET_PATH=$SHEET_PATH, X_ATOMS=$X_ATOMS, Y_ATOMS=$Y_ATOMS, DEFECTS=$DEFECTS, DEFECT_RANDOM_SEED=$DEFECT_RANDOM_SEED, ACCEPT_DUPES=$ACCEPT_DUPES, SIM_LENGTH=$SIM_LENGTH, TIMESTEP=$TIMESTEP, THERMO=$THERMO, DETAILED_DATA=$DETAILED_DATA, ANGLE_TESTING=$ANGLE_TESTING, THETA=$THETA, FRACTURE_WINDOW=$FRACTURE_WINDOW, STORAGE_PATH=$STORAGE_PATH, POTENTIAL=$POTENTIAL"
 
     fi
 }
@@ -55,15 +56,15 @@ send_email_notification() {
 # X_ATOMS="${X_ATOMS:-60}"
 # Y_ATOMS="${Y_ATOMS:-20}"
 # SHEET_PATH="/hpc/home/avb25/Graphene_Strength_Surface/simulation_data/data_files/data.${X_ATOMS}_${Y_ATOMS}"
-SHEET_PATH="${SHEET_PATH:-/hpc/home/avb25/projects/Graphene_Strength_Surface/data_files/data.60_60_rel1}"
-submit_simulation
+# SHEET_PATH="${SHEET_PATH:-/hpc/home/avb25/projects/Graphene_Strength_Surface/data_files/data.60_60_rel1}"
+# submit_simulation
 
 # submit_simulation 2935 # put sim_id number here if you want it to repeat a sim
 
 # submit_simulation 2926
 # submit_simulation 36907
 
-
+submit_simulation 91
 
 
 
